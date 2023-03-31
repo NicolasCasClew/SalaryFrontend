e8392e18b5bbdde5d0c46ff1cac0437077a4e7c1
Nicolas <nicolascasclew@gmail.com>
2023-03-31 13:54:29 +0200

    functionality added to inputs, they can modify the number in anoter conmponent

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,21 @@
 import { Layout, Menu, Slider, Select } from "antd";
 import "./App.css";
+import { useState } from "react";
 import { Test1 } from "./test1";
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const handleChange = (value: number) => {
-  console.log(`selected ${value + 7}`);
-};
-const sliderChange = (value: number) => {
-  console.log(`selected ${value}`);
-};
-
 function App() {
+  const [counter, setCounter] = useState(0);
+  const handleChange = (value: number) => {
+    console.log(`selected ${value}`);
+    setCounter(value);
+  };
+
+  const sliderChange = (value: number) => {
+    console.log(`selected ${value}`);
+    setCounter(value);
+  };
   return (
     <div>
       <Layout>
@@ -20,17 +24,18 @@ function App() {
             className="logo"
             src="https://adabtive.nl/wp-content/uploads/2023/02/logo2-100.png"
           />
-          <h1 style={{ fontSize: "25px" }}>Salary Tool</h1>
+          <h1 style={{ fontSize: "23px" }}>Salary Tool</h1>
         </Header>
         <Layout>
           <Sider className="siderStyle">Sider</Sider>
           <Content className="contentStyle">
             {" "}
-            <div>
+            <div style={{ minWidth: "450px" }}>
               <div className="selector">
                 <h4 className="txt">Text 1 Text</h4>
                 <Select
                   style={{ width: 120 }}
+                  defaultValue={1}
                   onChange={handleChange}
                   options={[
                     { value: 1, label: "1" },
@@ -45,6 +50,7 @@ function App() {
                 <Select
                   style={{ width: 120 }}
                   onChange={handleChange}
+                  defaultValue={1}
                   options={[
                     { value: 1, label: "1" },
                     { value: 2, label: "2" },
@@ -66,7 +72,7 @@ function App() {
                 </div>
               </div>
             </div>
-            <h1 style={{ padding: "15%" }}>mucho texto </h1>
+            <h1>{counter}</h1>
           </Content>
           <Sider className="siderStyle">Sider</Sider>
         </Layout>
