27d6e8a74a2cafdde81e37882df41e127f3c4580
Nicolas <nicolascasclew@gmail.com>
2023-05-24 18:15:29 +0200

    routing working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,44 +1,16 @@
-import { ConfigProvider, Layout, theme } from "antd";
-import { ReactElement, useState } from "react";
-import { Page } from "./page";
-import { SalaryHeader } from "./header";
-import styles from "./App.module.scss";
+import * as React from "react";
+//import { Routes, Route } from 'react-router-dom';
+import SalaryContent from "./SalaryContent";
+import DBContent from "./DBContent";
+import { Route, Routes } from "react-router-dom";
 
-const { Content, Footer, Sider } = Layout;
-
-function App(): ReactElement {
-  const [darkTheme, setDarkTheme] = useState(true);
+export default function App() {
   return (
-    <>
-      <ConfigProvider
-        theme={{
-          algorithm: darkTheme ? theme.darkAlgorithm : theme.defaultAlgorithm,
-        }}
-      >
-        <Layout>
-          <SalaryHeader setthemeChange={setDarkTheme}></SalaryHeader>
-          <Layout>
-            <Sider
-              breakpoint="xl"
-              width={"15vw"}
-              collapsedWidth={"1rem"}
-              className={styles["sider"]}
-            />
-            <Content className={styles["content"]}>
-              <Page></Page>
-            </Content>
-            <Sider
-              breakpoint="xl"
-              width={"15vw"}
-              collapsedWidth={"1rem"}
-              className={styles["sider"]}
-            />
-          </Layout>
-          <Footer className={styles["footer"]}></Footer>
-        </Layout>
-      </ConfigProvider>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<SalaryContent />} />
+        <Route path="admin" element={<DBContent />} />
+      </Routes>
+    </div>
   );
 }
-
-export default App;
---
+++ src/components/DBContent.tsx
@@ -0,0 +1,46 @@
+import { ConfigProvider, Layout, theme } from "antd";
+import { ReactElement, useState } from "react";
+import { Page } from "./page";
+import { SalaryHeader } from "./header";
+import styles from "./App.module.scss";
+import InfiniScroll from "./infiniList";
+
+const { Content, Footer, Sider } = Layout;
+
+function App(): ReactElement {
+  const [darkTheme, setDarkTheme] = useState(true);
+  return (
+    <>
+      <ConfigProvider
+        theme={{
+          algorithm: darkTheme ? theme.darkAlgorithm : theme.defaultAlgorithm,
+        }}
+      >
+        <Layout>
+          <SalaryHeader setthemeChange={setDarkTheme}></SalaryHeader>
+          <Layout>
+            <Sider
+              breakpoint="xl"
+              width={"15vw"}
+              collapsedWidth={"1rem"}
+              className={styles["sider"]}
+            />
+            <Content className={styles["content"]}>
+              {/* <Page></Page> */}
+              <InfiniScroll />
+            </Content>
+            <Sider
+              breakpoint="xl"
+              width={"15vw"}
+              collapsedWidth={"1rem"}
+              className={styles["sider"]}
+            />
+          </Layout>
+          <Footer className={styles["footer"]}></Footer>
+        </Layout>
+      </ConfigProvider>
+    </>
+  );
+}
+
+export default App;
